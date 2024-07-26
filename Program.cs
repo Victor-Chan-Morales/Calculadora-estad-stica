2139520c2c236bd68a39e0bf43870759520a4847
Víctor_3.1416 <matemorales2020@gmail.com>
2024-07-26 04:05:04 -0600

    Concluído

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace Calculadora_estadística
     {
         static int cantidadNumeros = 1;
         static double resultado = 0;
+        static string tipoDeMedida = "";
         static List<double> listaNumero = new List<double>();
         static void MostrarMenu()
         {
@@ -41,15 +42,15 @@ namespace Calculadora_estadística
             {
                 suma += numero;
             }
-            return suma / lista.Count;
+            return suma / lista.Count; //Count cuenta el número de elementos de una lista
         }
         static double CalcularMediana(List<double> lista)
         {
-            listaNumero.Sort();
+            listaNumero.Sort(); // sort ordena (clasifica) del menor al mayor los elementos de una lista 
             cantidadNumeros = listaNumero.Count;
             if (cantidadNumeros%2==1)
             {
-                return listaNumero[cantidadNumeros/ 2];
+                return listaNumero[cantidadNumeros/ 2]; //Regresa el elemento con indice de cantidadNumeros/2
             }
             else
             {
@@ -58,10 +59,46 @@ namespace Calculadora_estadística
                 return (listaNumero[mid1] + listaNumero[mid2]) / 2;
             }
         }
+        static List<double> CalcularModa(List<double> lista)
+        {
+            List<double> noRepetidos = lista.Distinct().ToList(); //obtiene los numeros no repetidos y los agrega en una lista 
+            List<int> repetidos = new List<int>();
+
+            foreach (double numero in noRepetidos)
+            {
+                int cuenta = lista.Count(x => x == numero);
+                repetidos.Add(cuenta);
+            }
+
+            int maxFrecuencia = repetidos.Max();
+            List<double> modas = new List<double>();
+
+            for (int i = 0; i < repetidos.Count; i++)
+            {
+                if (repetidos[i] == maxFrecuencia)
+                {
+                    modas.Add(noRepetidos[i]);
+                }
+            }
+            return modas;
+        }
+        static double CalcularDesviacion(List<double>lista)
+        {
+            double media=lista.Average(); //función para calcular la media
+            double sumaDesviacion = 0;
+            foreach (double numero in lista)
+            {
+                sumaDesviacion += Math.Pow(numero-media,2);
+            }
+            double division=sumaDesviacion/lista.Count();
+            return Math.Sqrt(division);
+        }
         static void MostrarResultado()
         {
             Console.Clear();
-            Console.WriteLine("De los números ingresados: "+listaNumero);
+            Console.WriteLine("De los números ingresados: ");
+            string cadena = string.Join(" ", listaNumero);
+            Console.WriteLine(cadena);
             Console.WriteLine("El resultado es: " + resultado);
         }
         static void Main(string[] args)
@@ -91,7 +128,26 @@ namespace Calculadora_estadística
                             Console.ReadKey();
                             break;
                         case 4:
+                            List<double> modas = CalcularModa(listaNumero);
+                            Console.Clear();
+                            Console.WriteLine("De los números ingresados: ");
+                            Console.WriteLine(string.Join(" ", listaNumero));
+                            Console.WriteLine("Las modas son: " + string.Join(", ", modas));
+                            Console.ReadKey();
+                            break;
+                        case 5:
+                            resultado=CalcularDesviacion(listaNumero);
                             MostrarResultado();
+                            Console.ReadKey();
+                            break;
+                        case 6:
+                            SolicitarListaNumeros();
+                            break;
+                        case 0:
+                            Console.WriteLine("Saliendo del programa...");
+                            return;
+                        default:
+                            Console.WriteLine("Esta opción no está disponible, seleccione una opción del menú");
                             Console.ReadKey();
                             break;
                     }
@@ -99,6 +155,21 @@ namespace Calculadora_estadística
                 catch (FormatException)
                 {
                     Console.WriteLine("Ha ingresado un caracter inválido, clic e intente de nuevo.");
+                    Console.ReadKey();
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    Console.WriteLine("La lista está vacía, debe ingresar primero los números.");
+                    Console.ReadKey();
+                }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("No puede realizar esta operación, está intentando dividir un número entre cero, clic e intente de nuevo.");
+                    Console.ReadKey();
+                }
+                catch(InvalidOperationException)
+                {
+                    Console.WriteLine("No puede realizar esta operación, no ha ingresado valores. Clic e intente de nuevo.");
                     Console.ReadKey();
                 }
             } while (opcionMenu > 0);
